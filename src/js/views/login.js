e2828c6dd3470db3845f8ef92c968966fd5e2a7b
Esteban Arias <estebanariaslima@gmail.com>
2022-03-24 18:43:47 -0400

    se agrega la vista muerta del login y se corrige el navbar

---
+++ src/js/views/login.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+import "../../styles/demo.css";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+return(
+    <div className="content_of_the_page">
+			<div className="container">
+				<h1 style={{ "fontSize": "50px", "fontWeight": "100", "fontFamily": "Spartan, sans-serif" }}>Inicia sesión</h1>
+				<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif" }}>para empezar a viajar :) 🌳🍂</p>
+                <div className="container d-flex justify-content-center row my-3">
+					<div id="form" className="justify-content-center d-flex row p-2">
+						<h1 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Correo electrónico</h1>
+						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Dirección de correo electrónico 📧</p>
+						<input placeholder="Arepa1234@mail.com"></input>
+						<h1 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Contraseña</h1>
+						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}></p>
+						<input placeholder="1234567890 🤡"></input>
+						<input style={{ "width": "500px" }} className="my-3 btn btn-primary" type="submit" value="Iniciar sesión"></input>
+					</div>
+				</div>
+            </div>
+        </div>)}
