6bb50e1ff1ab794dac83c30fd7fbc71cb8fa413a
Wilker Blanco <wilkerblanco18@gmail.com>
2022-03-27 14:30:02 -0400

    se agregaron las funciones al registro de viajeros

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -17,26 +17,66 @@ export const Demo = () => {
 					<div id="form" className="justify-content-center d-flex row p-2">
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Apodo</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Usaremos tu apodo para mostrarlo al publico, no uses tu nombre real⛺</p>
-						<input placeholder=""></input>
+						<input 
+							placeholder=""
+							type="text"
+							onChange={actions.handleInputChange}
+							name="nickname"
+						></input>
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Nombre</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Dejanos aqui tu nombre 🌾</p>
-						<input placeholder="Opcional"></input>
+						<input 
+							placeholder="Opcional"
+							type="text"
+							onChange={actions.handleInputChange}
+							name="name"
+						></input>
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Apellido</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Dejanos aqui tu apellido 🌵</p>
-						<input placeholder="Opcional"></input>
+						<input 
+							placeholder="Opcional"
+							type="text"
+							onChange={actions.handleInputChange}
+							name="lastname"
+						></input>
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Número de telefono</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Dejanos tu número de telefono para saber que eres una persona real 🤖</p>
-						<input placeholder=""></input>
+						<input 
+							placeholder=""
+							type="text"
+							onChange={actions.handleInputChange}
+							name="phone_number"
+						></input>
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Correo electrónico</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Dirección de correo electrónico 📧</p>
-						<input placeholder="Arepa1234@mail.com"></input>
+						<input 
+							placeholder="Arepa1234@mail.com"
+							type="text"
+							onChange={actions.handleInputChange}
+							name="email"
+							></input>
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Contraseña</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Se recomienda usar mayúsculas y números para más seguridad 🔒</p>
-						<input placeholder="1234567890 🤡"></input>
+						<input 
+							placeholder="1234567890 🤡"
+							type="text"
+							onChange={actions.handleInputChange}
+							name="password"
+							></input>
 						<h2 style={{ "paddingTop": "20px", "fontFamily": "Raleway, sans-serif" }}>Confirmar contraseña</h2>
 						<p style={{ "fontSize": "20px", "fontFamily": "Nunito, sans-serif", "paddingTop": "20px" }}>Confírmanos tu contraseña 🔐</p>
-						<input placeholder=""></input>
-						<input style={{ "width": "500px" }} className="my-3 btn btn-primary" type="button" value="Registrarme"></input>
+						<input 
+							placeholder=""
+							type="text"
+							onChange={actions.handleInputChange}
+							name="password_confirm"						
+						></input>
+						<input style={{ "width": "500px" }} className="my-3 btn btn-primary" 
+							type="button" 
+							value="Registrarme"
+							resource="/sing-in-traveler"
+					   		onClick={actions.handleSubmit}
+						></input>
 					</div>
 					<div className="d-flex justify-content-center align-items-center col">
 						<div className="lineas"></div>
