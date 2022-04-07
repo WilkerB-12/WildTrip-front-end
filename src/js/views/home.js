27ef67ba21c109c89c5d369befbf271e7459aace
Esteban Arias <estebanariaslima@gmail.com>
2022-04-06 21:45:35 -0400

    se cambian los colores del home.js para ser mas llamativo a la vista

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,18 +6,18 @@ import { Link } from "react-router-dom";
 
 
 export const Home = () => (
-	<>
+	<div className="content_of_the_page">
 		<div className="text-center my-5">
 			<Post />
 			<Post />
 			<Post />
 		</div>
-		<div className="mx-5 my-5" style={{"position":"fixed", "top":"65%", "left":"85%"}}>
+		<div className="mx-5 my-5" style={{ "position": "fixed", "top": "65%", "left": "85%" }}>
 			<Link to="create-post">
-			<div className="boton-para-crear-post">
-				<h1 style={{"display":"flex", "paddingTop":"12%", "justifyContent":"center", "color":"black"}}>+</h1>
-			</div>
+				<div className="boton-para-crear-post">
+					<h1 style={{ "display": "flex", "paddingTop": "12%", "justifyContent": "center", "color": "black" }}>+</h1>
+				</div>
 			</Link>
 		</div>
-	</>
+	</div>
 );
